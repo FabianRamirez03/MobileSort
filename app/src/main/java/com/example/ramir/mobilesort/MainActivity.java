5ca0da40c4704e2a1f7176f7d3a2feb835522b7f
[Fabian] <[ramirezfabian003@gmail.com]>
2018-10-04 07:04:46 -0600

    Created activities and pass between them

--- app/src/main/java/com/example/ramir/mobilesort/MainActivity.java
+++ app/src/main/java/com/example/ramir/mobilesort/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ramir.mobilesort;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void AnimationBubble (View view){
+        Intent animation = new Intent(this, BubbleAnimation.class);
+        startActivity(animation);
+    }
+    public void AnimationQuick (View view){
+        Intent animation = new Intent(this, AnimationActivity.class);
+        startActivity(animation);
     }
 }
