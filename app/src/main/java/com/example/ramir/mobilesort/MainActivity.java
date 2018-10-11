6fb81d7d4575301ed8a075121fe03b016fba61bd
toms99 <tomas.seguram@gmail.com>
2018-10-10 18:02:18 -0600

    SeekBar working but not synchronised with logic.

--- app/src/main/java/com/example/ramir/mobilesort/MainActivity.java
+++ app/src/main/java/com/example/ramir/mobilesort/MainActivity.java
@@ -4,16 +4,44 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.ramir.mobilesort.Sort.SortController;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private ProgressBar progressBar;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SortController.init(10);
+        textView = (TextView) findViewById(R.id.textView4);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setMax(30);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressBar.setProgress(progress + 5);
+                textView.setText("" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void AnimationBubble (View view){
