9ac9f1d8fb60437e054cca95cce81e472f2f7189
JoDaniel1412 <jodaniel1412@gmail.com>
2018-10-16 18:37:05 -0600

    Corrections to layouts.xml

--- app/src/main/java/com/example/ramir/mobilesort/QuickSortActivity.java
+++ app/src/main/java/com/example/ramir/mobilesort/QuickSortActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.ramir.mobilesort.Lists.SimpleList;
+import com.example.ramir.mobilesort.Sort.SortController;
 import com.example.ramir.mobilesort.draw.Drawer;
 
 public class QuickSortActivity extends AppCompatActivity {
@@ -22,11 +24,12 @@ public class QuickSortActivity extends AppCompatActivity {
         startActivity(goBack);
     }
 
-    public static void setMethod(String method) {
-        QuickSortActivity.method = method;
+    public void unSort(View view){
+        SortController controller = SortController.getInstance();
+        controller.unSortList(controller.getNumList());
     }
-
-
-
-
+    public void Sort(View view){
+        SimpleList numList = SortController.getInstance().getNumList();
+        //sortMethods.quickSort(numList);
+    }
 }
